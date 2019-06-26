a2fdf8d017069d3361793b163b16f38d15c36ef7
lesoaress <lemayara16@gmail.com>
2019-06-26 12:26:29 -0300

    criando o projeto

---
+++ projeto/simplemooc/simplemooc/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+#URL's do sistema
+urlpatterns = patterns('',
+    #Na primeira diz que para uma url vazia, execute a 
+    #função simplemooc.core.views.home e seu nome é home
+    url(r'^$', 'simplemooc.core.views.home', name='home'),
+    url(r'^admin/', include(admin.site.urls)),
+)
