f4822cfd77051672864d57d5a6cd05ba3550b178
lesoaress <lemayara16@gmail.com>
2019-06-27 13:32:47 -0300

    commitando a pagina contato

--- projeto/simplemooc/simplemooc/urls.py
+++ projeto/simplemooc/simplemooc/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     #Na primeira diz que para uma url vazia, execute a 
     #função simplemooc.core.views.home e seu nome é home
     url(r'^$', 'simplemooc.core.views.home', name='home'),
+    url(r'^contato/$', 'simplemooc.core.views.contact', name='contact'),
     url(r'^admin/', include(admin.site.urls)),
 )
